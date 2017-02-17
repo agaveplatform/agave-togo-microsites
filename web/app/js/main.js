4e25e52ace1f7bdce764ab6712c94c9efcde04ef
Rion Dooley <dooley@tacc.utexas.edu>
2017-02-17 11:47:05 -0600

    Updating bower.js to pin down identical versios fo angular and angular-animate. This was causing a loading issue.

--- web/app/js/main.js
+++ web/app/js/main.js
@@ -267,7 +267,7 @@ AgaveToGo.config(['$controllerProvider', function($controllerProvider) {
 AgaveToGo.controller('AppController', ['$scope', '$rootScope', function($scope, $rootScope) {
     $scope.$on('$viewContentLoaded', function() {
         App.initComponents(); // init core components
-        //Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive 
+        //Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive
     });
 
     $scope.$on('oauth:login', function(event, token) {
